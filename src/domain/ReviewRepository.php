b0b747256358905f8c35001192499fa4637bbcec
damouu <mouadsehbaoui@gmail.com>
2023-01-16 00:52:26 +0900

    adds the getReviewUuid function to retrieve through the mongoloid ODM the associated document in the mongo database.
    and adds also the function postReview to post a review document into the database.

--- src/domain/ReviewRepository.php
+++ src/domain/ReviewRepository.php
@@ -7,21 +7,28 @@ use src\model\Review;
 
 class ReviewRepository
 {
-    // le point d'interrogation, s'est pour Nullable types, qui veut dire que le type de donnees peut etre dans ce cas
-    // du type de la \stdClass  OU ALORS DE TYPE NULL
-    public function getReviewUuid(string $uuid): ?array
+    public function getReviewUuid(string $uuid): ?Review
     {
-        return Review::first($uuid)->toArray();
+        return Review::where(['uuid_review' => $uuid])->first();
     }
 
     public function postReview(string $data): void
     {
         $review = new Review();
-        $review->setStars(5);
-        $review->setComment('test');
+        $arrays = explode(',', $data);
+        foreach ($arrays as $array) {
+            $pepe = explode(':', $array);
+            $str_replace = str_replace(array('"', "'", '{',), ' ', $pepe[0]);
+            $trim = trim($str_replace);
+            $arra[$trim] = $pepe[1];
+        }
+        $review->setStars($arra['stars']);
+        $review->setComment($arra['comment']);
         $review->setUuidReview(Uuid::uuid4());
-        $review->setUuidUser(Uuid::uuid4()); // ici faudrais prendre le uuid du user dans le json web token
-        $review->setUuidItem(Uuid::uuid4());
+        $review->setUuidUser($arra['uuid_user']); // ici faudrais prendre le uuid du user dans le json web token
+        $review->setUuidItem($arra['uuid_item']);
+        $review->setIdItem($arra['id_item']);
+        $review->setdeleted_at('');
         $review->save();
     }
 
